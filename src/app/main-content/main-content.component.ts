b2fc59b2145c619286fcde00499f5e70365a49f4
Cihad Isen <chihad@live.de>
2024-04-24 02:34:25 +0200

    created navbar-component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
