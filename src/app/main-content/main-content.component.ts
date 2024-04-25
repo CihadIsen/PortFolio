a16313ec37ba46c252a66472b307b391063b17d0
Cihad Isen <chihad@live.de>
2024-04-25 03:16:28 +0200

    finished navbar, and started frontpage

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FrontPageComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
