6fbaba042dd1de63885c95900a7405d903b68faa
Cihad Isen <chihad@live.de>
2024-05-06 14:14:06 +0200

    addet contact formular

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,13 +3,14 @@ import { FrontPageComponent } from './front-page/front-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [ FrontPageComponent,AboutMeComponent, SkillsComponent, PortfolioComponent ],
+  imports: [ FrontPageComponent,AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
